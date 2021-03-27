b307df99ca20cc70198c207d66a6f0e87b9e7b73
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-03-27 22:51:14 +0300

    Да будет цвет!

---
+++ MIS.Infoboard/Converters/ResourceListItemStyleConverter.cs
@@ -0,0 +1,42 @@
+﻿using MIS.Application.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MIS.Infoboard.Converters
+{
+	public class ResourceListItemStyleConverter : IValueConverter
+	{
+		public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+		{
+			if (value is not ResourceViewModel resource)
+			{
+				return DependencyProperty.UnsetValue;
+			}
+
+			if (parameter is not String option || String.IsNullOrEmpty(option))
+			{
+				return DependencyProperty.UnsetValue;
+			}
+
+			var resourceKey = resource.Count switch
+			{
+				var count when count > 50 => "success",
+				var count when count > 25 => "warning",
+				var count when count > 0 => "danger",
+				var count when count == 0 => "secondary",
+				_ => "default"
+			};
+
+			var result = System.Windows.Application.Current.TryFindResource($"{resourceKey}{option}ListItem");
+
+			return result;
+		}
+
+		public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+	}
+}
---
+++ MIS.Infoboard/Converters/SpecialtyListItemStyleConverter.cs
@@ -0,0 +1,47 @@
+﻿using MIS.Application.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MIS.Infoboard.Converters
+{
+	public class SpecialtyListItemStyleConverter : IValueConverter
+	{
+		public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+		{
+			if (value is not SpecialtyViewModel specialty)
+			{
+				return DependencyProperty.UnsetValue;
+			}
+
+			if (parameter is not String option || String.IsNullOrEmpty(option))
+			{
+				return DependencyProperty.UnsetValue;
+			}
+
+			if (specialty.Resources == null || specialty.Resources.Length == 0)
+			{
+				return DependencyProperty.UnsetValue;
+			}
+
+			var resourceKey = (specialty.Count / specialty.Resources.Length) switch
+			{
+				var count when count > 50 => "success",
+				var count when count > 25 => "warning",
+				var count when count > 0 => "danger",
+				var count when count == 0 => "secondary",
+				_ => "default"
+			};
+
+			var result = System.Windows.Application.Current.TryFindResource($"{resourceKey}{option}ListItem");
+
+			return result;
+		}
+
+		public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+	}
+}
