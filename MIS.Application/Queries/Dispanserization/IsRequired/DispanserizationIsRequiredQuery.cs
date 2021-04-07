b2bedd3a18309b5399f5d54a85a618f49514e611
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-04-07 21:07:51 +0300

    copyright

--- MIS.Application/Queries/Dispanserization/IsRequired/DispanserizationIsRequiredQuery.cs
+++ MIS.Application/Queries/Dispanserization/IsRequired/DispanserizationIsRequiredQuery.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright © 2020 Vladimir Deryagin. All rights reserved
+﻿#region Copyright © 2020-2021 Vladimir Deryagin. All rights reserved
 /*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
